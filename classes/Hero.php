3c49d11ccd68c211b674cba7aab14b8aa90190dc
Cloud <ruben.as532@gmail.com>
2023-02-23 10:03:06 +0100

    Adapted Hero to secondary weapon support

--- classes/Hero.php
+++ classes/Hero.php
@@ -4,22 +4,34 @@
 		private float  $magic;
 		private string $name;
 		private Weapon $main_weapon;
+		private Weapon $secondary_weapon;
 		
-		public function __construct(float $life,float $magic,string $name,Weapon $main_weapon = null) 
+		public function __construct(float $life,float $magic,string $name,Weapon $main_weapon = null, Weapon $secondary_weapon = null) 
 		{
 			$this->life = $life;
 			$this->magic = $magic;
 			$this->name = $name;
 			$this->main_weapon = $main_weapon;
+			$this->secondary_weapon = $secondary_weapon;
 		}
 
-		public function setWeapon(Weapon $weapon) {
-			$this->weapon = $weapon;
+		public function setPrimaryWeapon(Weapon $weapon) {
+			$this->main_weapon = $weapon;
 		}
 
-		public function useWeapon() {
+		public function setSecondaryWeapon(Weapon $weapon) {
+			$this->secondary_weapon = $weapon;
+		}
+
+		public function usePrimaryWeapon() {
 			echo $this->name." ";
-			$this->weapon->attack();
+			$this->main_weapon->attack();
+			echo '<br>';
+		}
+
+		public function useSecondaryWeapon(Weapon $weapon) {
+			echo $this->name." ";
+			$this->secondary_weapon->attack();
 			echo '<br>';
 		}
 	}	
